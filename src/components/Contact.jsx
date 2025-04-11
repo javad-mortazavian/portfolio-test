339c7df8ce93385a2e535779be303649df9533c4
ItisAriya <your.email@example.com>
2025-04-11 12:27:55 -0400

    first commit

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -101,7 +101,7 @@ const Contact = () => {
     <div className={`xl:mt-12 flex xl:flex-row flex-col-reverse gap-10 overflow-hidden`}>
       <motion.div variants={slideIn("left", "tween", 0.2, 1)} className="flex-[0.75] bg-black-100 p-8 rounded-2xl">
         <p className={styles.sectionSubText}>Get in touch</p>
-        <h3 className={styles.sectionHeadText}>Contact Me</h3>
+        <h3 className={styles.sectionHeadText}>Contact US</h3>
 
         <form ref={formRef} onSubmit={handleSubmit} className="mt-12 flex flex-col gap-8">
           <InputField
