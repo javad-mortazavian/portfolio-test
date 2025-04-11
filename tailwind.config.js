76ca8e7e28ca6c0c78f3c62f61b9b6f4f684a650
ItisAriya <your.email@example.com>
2025-04-10 22:29:53 -0400

    first commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,26 +1,19 @@
-module.exports = {
-  content: ['./src/**/*.{js,jsx}'],
-  mode: 'jit',
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html", // Ensure your HTML file is included
+    "./src/**/*.{js,jsx,ts,tsx}", // Include all JS and JSX files in the src directory
+  ],
+  darkMode: 'class', // Enable dark mode using the 'class' strategy
   theme: {
     extend: {
-      colors: {
-        primary: '#050816',
-        secondary: '#aaa6c3',
-        tertiary: '#151030',
-        'black-100': '#100d25',
-        'black-200': '#090325',
-        'white-100': '#f3f3f3',
-      },
-      boxShadow: {
-        card: '0 35px 120px -15px #211e35',
-      },
-      screens: {
-        xs: '450px',
-      },
-      backgroundImage: {
-        'hero-pattern': `url(/herobg.png)`,
+      fontWeight: {
+        'extra-light': 200,
+        'medium': 500,
       },
     },
   },
-  plugins: [],
-};
+  plugins: [
+    require('flowbite/plugin'), // Include the Flowbite plugin
+  ],
+}
