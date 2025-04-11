8ae27b1028849f6f8715418219c7b8415673b0c1
ItisAriya <your.email@example.com>
2025-04-11 11:08:41 -0400

    first commit

--- vite.config.js
+++ vite.config.js
@@ -3,4 +3,13 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
+  base: "/portfolio-test",
+  optimizeDeps: {
+    include: ["flowbite"],
+  },
+  resolve: {
+    alias: {
+      '/vite.svg': '/src/vite.svg',
+    },
+  },
 });
