0cef82c2e2817e3a037c1d09a90136b92a0a835d
ItisAriya <your.email@example.com>
2025-04-10 22:21:14 -0400

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,31 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import Homepage from './pages/Homepage'
-import Navbar from './components/Navbar'
-import { Route, Routes } from 'react-router-dom'
-import Projects from './pages/Projects'
-import Publications from './pages/Publications'
-import Lab from './pages/Lab'
-import Repositories from './pages/Repositories'
-import Teaching from './pages/Teaching'
-import Team from './pages/Team'
-import Services from './pages/Services'
+import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
+import { About, Contact, Experience, Hero, Navbar, StarsCanvas, Tech, Works } from './components';
 
-
-function App() {
-  const [count, setCount] = useState(0)
-
+const App = () => {
   return (
-    <>
-      <Navbar />
-      <Routes>
-        <Route path="/" exact element={<Homepage />} />
-        <Route path="/projects" exact element={<Projects />} />
-        <Route path="/publications" exact element={<Publications />} />
-        <Route path="/lab" exact element={<Lab />} />
-        <Route path="/repositories" exact element={<Repositories />} />
-        <Route path="/teaching" exact element={<Teaching />} />
-        <Route path="/team" exact element={<Team />} />
-        <Route path="/services" exact element={<Services />} />
-      </Routes>
-    </>
-  )
-}
+    <BrowserRouter
+      future={{
+        v7_startTransition: true,
+        v7_relativeSplatPath: true,
+      }}
+    >
+      <div className="relative z-0 bg-primary">
+        <div className="bg-hero-pattern bg-cover bg-no-repeat bg-center">
+          <Navbar />
+          <Hero />
+        </div>
+        <About />
+        <Experience />
+        <Tech />
+        <Works />
+        <div className="relative z-0">
+          <Contact />
+          <StarsCanvas />
+        </div>
+      </div>
+    </BrowserRouter>
+  );
+};
 
-export default App
+export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -1,19 +1,13 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
-import 'flowbite';
-import './assets/style.css';
-import { BrowserRouter } from 'react-router-dom';
-// import './themeToggle';
-// import './qualification';
-// import './recentWorks';
-// import './techSkills';
-// import './headerTags';
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </StrictMode>,
-)
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import './index.css';
+
+const rootElement = document.getElementById('root');
+const root = ReactDOM.createRoot(rootElement);
+
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
