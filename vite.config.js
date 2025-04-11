40de23ac0b49f1ecbb54e2e3be7923326bd6da61
ItisAriya <your.email@example.com>
2025-04-10 22:44:12 -0400

    first commit

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,15 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: '/portfolio-test', // Ensure this matches your repository name
+  base: "/portfolio-test",
+  optimizeDeps: {
+    include: ["flowbite"],
+  },
+  resolve: {
+    alias: {
+      '/vite.svg': '/src/vite.svg',
+    },
+  },
 });
