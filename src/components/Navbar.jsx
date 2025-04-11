285981c0696b680197d6ddc2f677c79ea9131508
ItisAriya <your.email@example.com>
2025-04-11 12:53:41 -0400

    first commit

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -61,10 +61,10 @@ const Navbar = () => {
               window.scrollTo(0, 0);
             }}
           >
-            <img src={logo} alt="logo" className="w-9 h-9 object-contain" />
+            <img src={logo} alt="logo" className="w-14 h-14 object-contain" />
             <p className="text-white text-[20px] font-bold cursor-pointer flex">
-              LOHIT&nbsp;
-              <span className="sm:block hidden">KOLLURI</span>
+              M3TFluiD&nbsp;
+              <span className="sm:block hidden">Lab</span>
             </p>
           </Link>
           {renderNavLinks(false)}
