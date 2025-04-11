426276aba8ee66c41d0047482eeafd6f08fd417a
ItisAriya <your.email@example.com>
2025-04-11 19:28:06 -0400

    first commit

--- src/assets/index.js
+++ src/assets/index.js
@@ -5,17 +5,12 @@ import eduskill from "./company/eduskill.png";
 
 // Import tech stack images
 import img1 from "./tech-stack/img1.jpg";
-import img2 from "./tech-stack/img2.jpg";
 import img3 from "./tech-stack/img3.png";
 
 
 // Import project images
-import weatherpedia from "./projects/WeatherPedia.png";
-import termpw from "./projects/termpw.png";
-import payloadmaster from "./projects/payloadmaster.png";
-import mhft from "./projects/mhft.png";
-import sketcher from "./projects/sketcher.png";
-import CompileVortex from "./projects/CompileVortex.png";
+import MFSM from "./projects/MFSM.png";
+
 
 // Import tech logos
 import OpenFOAM from "./tech/OpenFOAM.png";
@@ -34,20 +29,14 @@ export {
   mathwork,
   edunet,
   img1,
-  img2,
   img3,
   github,
   OpenFOAM,
   Ansys,
   WRF,
   TF,
+  MFSM,
   PyTorch,
-  weatherpedia,
-  termpw,
-  payloadmaster,
-  mhft,
-  sketcher,
-  CompileVortex,
   logo,
   menu,
   close,
--- src/constants/index.js
+++ src/constants/index.js
@@ -1,15 +1,9 @@
 import {
   img1,
-  img2,
   img3,
+  MFSM,
   edunet,
-  weatherpedia,
-  termpw,
-  payloadmaster,
-  mhft,
-  sketcher,
   mathwork,
-  CompileVortex,
   eduskill,
   OpenFOAM,
   Ansys,
@@ -34,9 +28,8 @@ export const navLinks = [
 ];
 
 export const services = [
-  { title: "Neighbouhood", icon: img1 },
-  { title: "HR buildin", icon: img2 },
-  { title: "HR building", icon: img3 },
+  { title: "Neighborhood", icon: img1 },
+  { title: "High-rise building", icon: img3},
 
 ];
 
@@ -50,76 +43,14 @@ export const technologies = [
 
 export const projects = [
   {
-    name: "WeatherPedia",
+    name: "MFSM",
     description:
-      "Web-based platform that allows users to access weather information for their location by entering it in the search field",
+      "A Multi-Fidelity Surrogate Model for the prediction of wind loads on high-rise buildings using OpenFOAM and PyTorch.",
     tags: [
-      { name: "Javascript", color: "blue-text-gradient" },
-      { name: "HTML", color: "green-text-gradient" },
-      { name: "bootstrap 5.3.0", color: "pink-text-gradient" },
-      { name: "Weather API by API Ninjas", color: "yellow-text-gradient" },
+      { name: "OpenFOAM", color: "blue-text-gradient" },
+      { name: "PyTorch", color: "green-text-gradient" },
     ],
-    image: weatherpedia,
-    source_code_link: "https://github.com/lohitkolluri/WeatherPedia",
-  },
-  {
-    name: "Terminal Like Portfolio Website",
-    description:
-      "A terminal themed portfolio website that allows users to type into the terminal and use commands like a real terminal.",
-    tags: [
-      { name: "HTML", color: "blue-text-gradient" },
-      { name: "css", color: "green-text-gradient" },
-      { name: "Javascript", color: "pink-text-gradient" },
-    ],
-    image: termpw,
-    source_code_link: "https://github.com/lohitkolluri/lohitkolluri.github.io",
-  },
-  {
-    name: "Mental Health Fitness Tracker",
-    description:
-      "ML model that utilizes regression techniques to provide insights into mental health and make predictions based on the available data.",
-    tags: [
-      { name: "Machine Learning", color: "blue-text-gradient" },
-      { name: "Jupyter Notebook", color: "green-text-gradient" },
-      { name: "Regression Algorithms", color: "pink-text-gradient" },
-    ],
-    image: mhft,
-    source_code_link:
-      "https://github.com/lohitkolluri/mental_health_fitness_tracker",
-  },
-  {
-    name: "PayloadMaster",
-    description:
-      "Tool to automate payload creation using the Metasploit framework",
-    tags: [
-      { name: "shell", color: "blue-text-gradient" },
-    ],
-    image: payloadmaster,
-    source_code_link: "https://github.com/lohitkolluri/PayloadMaster",
-  },
-  {
-    name: "CompileVortex",
-    description:
-      "Tool to automate payload creation using the Metasploit framework",
-    tags: [
-      { name: "Javascript", color: "blue-text-gradient" },
-      { name: "CSS", color: "green-text-gradient" },
-      { name: "HTML", color: "pink-text-gradient" },
-    ],
-    image: CompileVortex,
-    source_code_link: "https://github.com/lohitkolluri/CompileVortex",
-  },
-  {
-    name: "Sketcher",
-    description:
-      "Convert an input image to a pencil sketch using OpenCV and Matplotlib libraries.",
-    tags: [
-      { name: "OpenCV", color: "blue-text-gradient" },
-      { name: "Matplotlib", color: "green-text-gradient" },
-      { name: "Python", color: "pink-text-gradient" },
-    ],
-
-    image: sketcher,
-    source_code_link: "https://github.com/lohitkolluri/Image_to_Pencil_Sketch_App",
+    image: MFSM,
+   // source_code_link: "https://github.com/lohitkolluri/WeatherPedia",
   },
 ];
