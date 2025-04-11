87087475f5d5a738897c3806b50d02a736877f77
ItisAriya <your.email@example.com>
2025-04-10 22:43:13 -0400

    first commit

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/Website-/', // Ensure this matches your repository name
+  base: '/portfolio-test', // Ensure this matches your repository name
 });
